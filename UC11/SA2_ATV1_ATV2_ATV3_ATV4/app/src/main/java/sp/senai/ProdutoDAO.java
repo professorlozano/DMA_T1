bd2570a91405da5ae05923416d986f29c5d3175a
professorlozano <luizmlozano@gmail.com>
2022-07-02 14:49:41 -0300

    atividades 3 e 4 da SA2

--- UC11/SA2_ATV1_ATV2_ATV3_ATV4/app/src/main/java/sp/senai/ProdutoDAO.java
+++ UC11/SA2_ATV1_ATV2_ATV3_ATV4/app/src/main/java/sp/senai/ProdutoDAO.java
@@ -2,7 +2,11 @@ package sp.senai;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProdutoDAO {
     private Conexao conexao;
@@ -19,5 +23,19 @@ public class ProdutoDAO {
         values.put("quantidade",produto.getQuantidade());
         values.put("preco",produto.getPreco());
         return banco.insert("produtos",null,values);
+    }
+
+    public List<Produto> obterTodos(){
+        List<Produto> produtos = new ArrayList<>();
+        Cursor cursor = banco.query("produtos",new String[]{"id","nome","quantidade","preco"},null,null,null,null,null);
+        while(cursor.moveToNext()){
+            Produto p = new Produto();
+            p.setId(cursor.getInt(0));
+            p.setNome(cursor.getString(1));
+            p.setQuantidade(Integer.parseInt(cursor.getString(2)));
+            p.setPreco(Double.parseDouble(cursor.getString(3)));
+            produtos.add(p);
+        }
+        return produtos;
     }
 }
